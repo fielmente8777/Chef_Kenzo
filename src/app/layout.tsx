3ed51d6cb7cb203bb940cd32bb3e214f1f857a2d
abhijeet <abhijeet@eazotel.com>
2024-09-03 21:44:18 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        {/* <Script
           id="clarity-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -27,9 +27,45 @@ export default function RootLayout({
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "nqorxhu8vm");`,
           }}
+        /> */}
+
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PT5DTZ4X');
+            `,
+          }}
+        />
+
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-16573044562');`,
+          }}
         />
       </head>
       <body suppressHydrationWarning={true}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PT5DTZ4X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <NavBar />
         {children}
         <Footer />
