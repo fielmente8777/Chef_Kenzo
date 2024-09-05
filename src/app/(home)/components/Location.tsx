2844aabd2beb2eea7a711066054592cef620a303
abhijeet <abhijeet@eazotel.com>
2024-09-05 10:00:39 +0530

    updated as per the client

--- src/app/(home)/components/Location.tsx
+++ src/app/(home)/components/Location.tsx
@@ -16,7 +16,6 @@ const Location: React.FC<{ title: string; src: string }> = ({ title, src }) => {
         <div className="flex justify-center mt-8">
           <Link
             href="#contact"
-            target="_blank"
             className="flex gap-2 px-6 capitalize bg-red-primary hover:bg-white hover:text-red-primary hover:scale-x-110 duration-700 transition rounded-md py-3 font-medium border border-solid border-red-primary bg-primary text-black-primary"
           >
             Get in Touch
