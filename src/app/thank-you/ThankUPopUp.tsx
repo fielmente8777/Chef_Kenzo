ac3596ba9170c94148d1154c8f074d346d710390
sumit <sk7627577@gmail.com>
2024-09-18 16:44:21 +0530

    update

--- src/app/thank-you/ThankUPopUp.tsx
+++ src/app/thank-you/ThankUPopUp.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import src from "../../../public/images/contact1.jpg";
 import { useEffect } from "react";
+import { Container } from "@/components";
 const ThankUPopUp = () => {
   const router = useRouter();
 
@@ -11,7 +12,7 @@ const ThankUPopUp = () => {
   };
   useEffect(() => {
     // Google Ads Conversion Tracking
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.innerHTML = `
       gtag('event', 'conversion', {'send_to': 'AW-16573044562/haV-CM38_88ZENKu0t49'});
     `;
@@ -20,28 +21,32 @@ const ThankUPopUp = () => {
   return (
     <section>
       <div className="max-w-[1900px] mx-auto">
-        <div className="relative w-full h-screen aspect-[16/9]">
+        <div className="relative w-full aspect-[16/7] max-md:aspect-[4/5]">
           <Image
             src={src}
             alt="hospitality marketing"
             fill
             className="object-cover"
           />
-          <div className="absolute flex flex-col lg:items-start px-5 md:p-24 lg:justify-start justify-center h-screen w-full bg-[rgba(0,0,0,0.5)]">
-            <h1 className="text-5xl font-bold text-white mb-4">
-              Thank You!
-            </h1>
-            <p className="text-[24px] text-white/80 mb-8 leading-12  text-start">
-              Your submission has been received.
-              <br className="hidden lg:block" />
-              We will get back to you shortly.
-            </p>
-            <button
-              className="bg-blue-dark border text-white py-3 px-6 text-2xl rounded-md hover:bg-blue-800 transition duration-200"
-              onClick={redirectToHome}
-            >
-              Go to Home
-            </button>
+          <div className="absolute top-0 left-0 aspect-[16/7] max-md:aspect-[4/5] w-full bg-[rgba(0,0,0,0.5)] flex items-center">
+            <Container>
+              <div className="flex flex-col lg:items-start lg:justify-start justify-center w-full h-full">
+                <h1 className="text-5xl font-bold text-white mb-4">
+                  Thank You!
+                </h1>
+                <p className="text-[24px] text-white/80 mb-8 leading-12  text-start">
+                  Your submission has been received.
+                  <br className="hidden lg:block" />
+                  We will get back to you shortly.
+                </p>
+                <button
+                  className="bg-blue-dark border text-white py-3 px-6 text-2xl rounded-md hover:bg-blue-800 transition duration-200"
+                  onClick={redirectToHome}
+                >
+                  Go to Home
+                </button>
+              </div>
+            </Container>
           </div>
         </div>
       </div>
@@ -50,5 +55,3 @@ const ThankUPopUp = () => {
 };
 
 export default ThankUPopUp;
-
-
