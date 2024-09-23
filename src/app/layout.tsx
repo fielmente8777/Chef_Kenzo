b464245cb7d5a3429b31bc6d4835246a07d9c9c1
abhijeet <abhijeet@eazotel.com>
2024-09-23 13:46:49 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,30 @@ export default function RootLayout({
           }}
         />
 
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+                  if(f.fbq)return;n=f.fbq=function(){
+                  n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '822189070130523');
+              fbq('track', 'PageView');
+          `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=822189070130523&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <Script
           id="google-tag-manager"
           strategy="afterInteractive"
@@ -65,6 +89,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             `,
           }}
         />
+
+
+
       </head>
       <body suppressHydrationWarning={true}>
         <NavBar />
@@ -74,7 +101,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <Call />
 
 
-        
+
         {/* <!-- Google Tag Manager (noscript) --> */}
         <noscript>
           <iframe
